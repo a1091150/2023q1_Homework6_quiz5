39d5b330488c1dfe850af559c378e5d453f67ce6
DokiDokiPB <20497934+a1091150@users.noreply.github.com>
2023-04-08 00:30:35 +0800

    Fix intentation

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include "mpool.h"
+#include <stdio.h>
 
-int main()
-{
-    return 0;
+#define KB256 (65536 << 4)
+int main() {
+  const char arr[KB256];
+  bool is_good = pool_init((void *)arr, KB256);
+  if (is_good) {
+    printf("Hello World!");
+  } else {
+    goto final;
+  }
+final:
+  return 0;
 }
