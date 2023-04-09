26f16bbe38271dfe1e987ad9b6e2300eb2afbebe
DokiDokiPB <20497934+a1091150@users.noreply.github.com>
2023-04-09 21:22:18 +0800

    Add test function of alloc and free function

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
-#include "mpool.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include "mpool.h"
 
-#define KB256 (65536 << 4)
-int main() {
-  const char arr[KB256];
-  bool is_good = pool_init((void *)arr, KB256);
-  if (is_good) {
-    printf("Hello World!");
-  } else {
-    goto final;
-  }
-final:
-  return 0;
+#define KB256 (32 << 4)
+int main()
+{
+    char *arr = malloc(sizeof(char) * KB256);
+    bool is_good = pool_init((void *) arr, KB256);
+    int *a1 = pool_malloc(sizeof(int) * 8);
+    int *a2 = pool_malloc(sizeof(int) * 8);
+    pool_free(a1);
+    pool_free(a2);
+    return 0;
 }
